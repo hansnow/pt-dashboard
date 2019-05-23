60de9745c9aec3b3b32bffed22e7f9e68f6112d7
hansnow <hansnow2012@gmail.com>
2019-05-23 10:55:49 +0800

    Feat: add background image for login page

--- frontend/src/routes/Login.js
+++ frontend/src/routes/Login.js
@@ -46,61 +46,63 @@ function Login({ history }) {
   const userIcon = <Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />
   const lockIcon = <Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />
   return (
-    <div className="login-wrap">
-      <Tabs tabBarStyle={{ border: 'none' }}>
-        <Tabs.TabPane tab="登陆" key="login">
-          <Input
-            allowClear
-            placeholder="用户名"
-            prefix={userIcon}
-            value={loginUsername}
-            onChange={e => setLoginUsername(e.target.value)}
-          />
-          <Input.Password
-            allowClear
-            placeholder="密码"
-            prefix={lockIcon}
-            style={{ marginTop: 8 }}
-            value={loginPassword}
-            onChange={e => setLoginPassword(e.target.value)}
-            onPressEnter={login}
-          />
-          <Button
-            type="primary"
-            loading={loading}
-            style={{ marginTop: 8, width: '100%' }}
-            onClick={login}
-          >
-            登陆
-          </Button>
-        </Tabs.TabPane>
-        <Tabs.TabPane tab="注册" key="register">
-          <Input
-            allowClear
-            placeholder="用户名"
-            prefix={userIcon}
-            value={registerUsername}
-            onChange={e => setRegisterUsername(e.target.value)}
-          />
-          <Input.Password
-            allowClear
-            placeholder="密码"
-            prefix={lockIcon}
-            style={{ marginTop: 8 }}
-            value={registerPassword}
-            onChange={e => setRegisterPassword(e.target.value)}
-            onPressEnter={register}
-          />
-          <Button
-            type="primary"
-            loading={loading}
-            style={{ marginTop: 8, width: '100%' }}
-            onClick={register}
-          >
-            注册
-          </Button>
-        </Tabs.TabPane>
-      </Tabs>
+    <div className="login-bg">
+      <div className="login-wrap">
+        <Tabs tabBarStyle={{ border: 'none' }}>
+          <Tabs.TabPane tab="登陆" key="login">
+            <Input
+              allowClear
+              placeholder="用户名"
+              prefix={userIcon}
+              value={loginUsername}
+              onChange={e => setLoginUsername(e.target.value)}
+            />
+            <Input.Password
+              allowClear
+              placeholder="密码"
+              prefix={lockIcon}
+              style={{ marginTop: 8 }}
+              value={loginPassword}
+              onChange={e => setLoginPassword(e.target.value)}
+              onPressEnter={login}
+            />
+            <Button
+              type="primary"
+              loading={loading}
+              style={{ marginTop: 8, width: '100%' }}
+              onClick={login}
+            >
+              登陆
+            </Button>
+          </Tabs.TabPane>
+          <Tabs.TabPane tab="注册" key="register">
+            <Input
+              allowClear
+              placeholder="用户名"
+              prefix={userIcon}
+              value={registerUsername}
+              onChange={e => setRegisterUsername(e.target.value)}
+            />
+            <Input.Password
+              allowClear
+              placeholder="密码"
+              prefix={lockIcon}
+              style={{ marginTop: 8 }}
+              value={registerPassword}
+              onChange={e => setRegisterPassword(e.target.value)}
+              onPressEnter={register}
+            />
+            <Button
+              type="primary"
+              loading={loading}
+              style={{ marginTop: 8, width: '100%' }}
+              onClick={register}
+            >
+              注册
+            </Button>
+          </Tabs.TabPane>
+        </Tabs>
+      </div>
     </div>
   )
 }
