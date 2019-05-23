5a8f6171352a57ad5e049690608bb7309d1c464c
hansnow <hansnow2012@gmail.com>
2019-05-23 10:35:44 +0800

    Chore: edit running port

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,9 @@ import * as session from 'koa-session'
 import * as bodyParser from 'koa-bodyparser'
 
 import { routes, allowedMethods } from './routes'
+
+const PORT = 4000
+
 const app = new Koa()
 app.keys = ['l7ry%2=+$0eevbeu5s1aw^_(*t$h0yg!b4h2i+_^(l@o_#(6zz']
 
@@ -14,6 +17,6 @@ app.use(bodyParser())
 
 app.use(routes()).use(allowedMethods())
 
-app.listen(4000, () => {
-  console.log('app started at port 3000')
+app.listen(PORT, () => {
+  console.log('app started at port ' + PORT)
 })
